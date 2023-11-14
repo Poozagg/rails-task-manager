837a8fb51db24b6de363c06a0bc086dd455d5f09
Pooja Gurung <pooja_grg07@hotmail.com>
2023-11-14 15:43:15 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # LIST ALL THE TASKS
+  get 'tasks', to: 'tasks#index'
+
+  #   2 steps to create new task 1) get the creation form 2)post the parameters to create new task
+  # 1) GET THE FORM
+  get 'tasks/new', to: 'tasks#new'
+  # 2) POST THE NEW TASK
+  post 'tasks', to: 'tasks#create'
+
+  # SHOW A TASK
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # EDIT A TASK
+  # 1) CREATE FORM + GET THE TASK BY ID
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # 2) PATCH/UPDATE THE TASK WITH THE PARAMS OF THE FORM
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE A TASK FROM EXISTING LIST
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
 end
